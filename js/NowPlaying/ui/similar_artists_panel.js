c9d1b9309516bbd9b290c982fe246d4004b2ac4b
Gareth Andrew <gingerhendrix@hotmail.com>
2008-07-04 02:39:21 +0100

    Added image-quilts for similar artists and top albums panels

--- js/NowPlaying/ui/similar_artists_panel.js
+++ js/NowPlaying/ui/similar_artists_panel.js
@@ -11,13 +11,38 @@ Utils.namespace("NowPlaying.ui", {
     this.onChange = function(similar_artists){
       element.innerHTML = "";
 
-      similar_artists.slice(0, 10).forEach(function(sim){
+      function makeThumbnail(artist, clazz, size, label){
         var similar_li = document.createElement("li");
-        similar_li.innerHTML = sim.name + " (" + sim.match + "%)";
+        similar_li.setAttribute("class", clazz);
 
-        //var similarity_bar = create_bar(sim.match/100);
-        //similar_li.appendChild(similarity_bar);
-     
+        var artist_img = document.createElement("img");
+        artist_img.src = artist.image;
+        artist_img.title = artist.name + " (" + artist.match + "%)";
+        artist_img.width = size;
+        artist_img.height = size;
+        similar_li.appendChild(artist_img);
+
+        if(label){
+          var artist_label = document.createElement("span");
+          artist_label.innerHTML = artist.name + " (" + artist.match + "%)";
+          similar_li.appendChild(artist_label);
+        }
+        
+        return similar_li;
+      }
+            
+      similar_artists.slice(0, 4).forEach(function(sim){
+        var similar_li = makeThumbnail(sim, "large", 130, false);
+        $("similar_artists_list").appendChild(similar_li);
+      });
+      
+      similar_artists.slice(4, 20).forEach(function(sim){
+        var similar_li = makeThumbnail(sim, "medium", 65, false);
+        $("similar_artists_list").appendChild(similar_li);
+      });
+      
+      similar_artists.slice(20, 84).forEach(function(sim){
+        var similar_li = makeThumbnail(sim, "small", 33, false);
         $("similar_artists_list").appendChild(similar_li);
       });
     
--- js/NowPlaying/ui/top_albums_panel.js
+++ js/NowPlaying/ui/top_albums_panel.js
@@ -10,9 +10,16 @@ Utils.namespace("NowPlaying.ui", {
       element.innerHTML = "";
       var max_reach = top_albums[0].reach;
       
-      top_albums.slice(0, 10).forEach(function(album){
-        album_li = document.createElement("li");
-        album_li.innerHTML = album.name + " (" + Math.round(100*(album.reach/max_reach)) + "%)";
+      top_albums.slice(0, 8).forEach(function(album){
+        var album_li = document.createElement("li");
+        var album_img = document.createElement("img");
+        album_img.src = album.image_large;
+        album_img.width = 130;
+        album_img.height = 130;
+        var album_label = document.createElement("span");
+        album_label.innerHTML = album.name + " (" + Math.round(100*(album.reach/max_reach)) + "%)";
+        album_li.appendChild(album_img);
+        album_li.appendChild(album_label);
         //var reach_bar = create_bar((album.reach/max_reach));
         //album_li.appendChild(reach_bar);
         element.appendChild(album_li);
