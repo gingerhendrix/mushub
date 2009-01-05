cd2632704a3d3776d7e3a7e619ef9d3dc91aadb0
Gareth Andrew <gingerhendrix@hotmail.com>
2009-01-05 00:07:05 +0000

    Added home tab to application ui

--- js/NowPlaying/ui/application_ui.js
+++ js/NowPlaying/ui/application_ui.js
@@ -10,32 +10,21 @@ Utils.namespace("NowPlaying.ui", {
     var Data = NowPlaying.data;
     var appUI = Utils.namespace("NowPlaying.Application.ui");
     var appData = NowPlaying.Application.data;
-     
-   /* this.topbar = new Panels.UsernamePanel({
-          region:'north',
-        	width: 800,
-    });  
-     */
+
     this.userPanel = new Panels.UserPanel({datasource : datasource.user});        
-    
     this.searchPanel = new Panels.SearchPanel();
-    
-  //  var datasource1 = new Data.ArtistTabDatasource("Twisted Sister", "c6122fee-089f-41c4-a34f-e5f7e5607b05");
-  //  var datasource2 = new Data.ArtistTabDatasource("The Flower Kings", "0a389268-6fd8-4f8c-ab6e-0dba5ecec66b");
     
     this.tabPanel = new Ext.TabPanel({
           region : 'center',
           margins: '20 20 0 20',
           activeTab : 0,
           deferredRender : false,
-          items : [new Tabs.ArtistTab({title : "Now Playing", 
+          items : [ new Tabs.HomeTab(),
+                    new Tabs.ArtistTab({title : "Now Playing", 
                                        datasource : this.datasource.now_playing_tab,
                                        closable : false }),
                   ]
     });
-    
-   // datasource1.update();
-   // datasource2.update();
     
     this.viewport = new Ext.Viewport({
                layout : 'border',
