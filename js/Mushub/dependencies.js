2b018dbc3163dc345a9da33bbee40cb2b754e9bd
Gareth Andrew <gingerhendrix@hotmail.com>
2009-04-09 00:38:42 +0100

    Moving datasources to separate repo

--- js/Mushub/dependencies.js
+++ js/Mushub/dependencies.js
@@ -4,9 +4,9 @@
         if(isLocalhost()){
           loadLibrary(".", ["application"]);
           loadLibrary("model", ["artist", "artist_query"]);
-          loadLibrary("model/audioscrobbler", ["top_albums_datasource", "similar_artists_datasource"]);
-          loadLibrary("model/musicbrainz", ["artist_members_datasource", "artist_urls_datasource", "artist_search_datasource"]);
-          loadLibrary("model/wikipedia", ["wikipedia_datasource"]);
+          loadLibrary("../../../mushub_services_clients/js/services/audioscrobbler", ["top_albums_datasource", "similar_artists_datasource"]);
+          loadLibrary("../../../mushub_services_clients/js/services/musicbrainz", ["artist_members_datasource", "artist_urls_datasource", "artist_search_datasource"]);
+          loadLibrary("../../../mushub_services_clients/js/services/wikipedia", ["wikipedia_datasource"]);
 
           loadLibrary("ui", [ "content_panel", "artist_ui"]);
           loadLibrary("ui/panels", ["wikipedia_biography_panel", "lastfm_top_albums_panel", "lastfm_similar_artists_panel", "musicbrainz_links_panel"]);
